ab55135c238091202479af22bb9a70b9eaa3eaf2
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-02-13 20:26:47 -0300

    script ok

---
+++ Projeto-Cards/src/scripts/script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(".card__hidden").hide();
+    $("#details-1").click(function(){
+        $("#card__hidden1").show();
+    })
+
+    $("#details-2").click(function(){
+        $("#card__hidden2").show();
+    })
+
+    $("#details-3").click(function(){
+        $("#card__hidden3").show()
+    })
+
+    $("#details-4").click(function(){
+        $("#card__hidden4").show()
+    })
+
+    $("#details-5").click(function(){
+        $("#card__hidden5").show()
+    })
+
+    $("#details-6").click(function(){
+        $("#card__hidden6").show()
+    })
+
+    $("#details-7").click(function(){
+        $("#card__hidden7").show()
+    })
+
+    $("#details-8").click(function(){
+        $("#card__hidden8").show()
+    })
+
+    $("#details-9").click(function(){
+        $("#card__hidden9").show()
+    })
+
+    $(".__close").click(function(){
+        $(".card__hidden").hide();
+    })
+
+});
