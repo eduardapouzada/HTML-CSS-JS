879f4dd0e31e933ec0de042860abf3cf23b786cc
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-02-13 21:32:25 -0300

    body hidden

--- Projeto-Cards/src/scripts/script.js
+++ Projeto-Cards/src/scripts/script.js
@@ -2,42 +2,52 @@ $(document).ready(function(){
     $(".card__hidden").hide();
     $("#details-1").click(function(){
         $("#card__hidden1").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-2").click(function(){
         $("#card__hidden2").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-3").click(function(){
-        $("#card__hidden3").show()
+        $("#card__hidden3").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-4").click(function(){
-        $("#card__hidden4").show()
+        $("#card__hidden4").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-5").click(function(){
-        $("#card__hidden5").show()
+        $("#card__hidden5").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-6").click(function(){
-        $("#card__hidden6").show()
+        $("#card__hidden6").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-7").click(function(){
-        $("#card__hidden7").show()
+        $("#card__hidden7").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-8").click(function(){
-        $("#card__hidden8").show()
+        $("#card__hidden8").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $("#details-9").click(function(){
-        $("#card__hidden9").show()
+        $("#card__hidden9").show();
+        $('body').css('overflow', 'hidden');
     })
 
     $(".__close").click(function(){
         $(".card__hidden").hide();
+        $('body').css('overflow', 'auto');
     })
 
 });
