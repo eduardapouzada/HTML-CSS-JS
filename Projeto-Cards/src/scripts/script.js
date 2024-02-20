ff38f1bcdec5518cd1a41dcbb227d5b4e2528361
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-02-19 21:58:28 -0300

    div buy hidden

--- Projeto-Cards/src/scripts/script.js
+++ Projeto-Cards/src/scripts/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     $(".card__hidden").hide();
+    $(".buy__hidden").hide();
+
 
     $("[id^='details-']").click(function(){
         var id = $(this).attr('id').split('-')[1];
@@ -9,14 +11,19 @@ $(document).ready(function(){
 
     $(".__close").click(function(){
         $(".card__hidden").hide();
+        $(".buy__hidden").hide();
         $('body').css('overflow', 'auto');
     });
 
     $("[class^='card__button']").click(function(){
-        $(".hidden__buy").show();
+        $(".buy__hidden").show();
     });
 
-
+    $("[id^='card-precco']").click(function(){
+        var id = $(this).attr('id').split('-')[1];
+        $("#card__hidden" + id).show();
+        $('body').css('overflow', 'hidden');
+    });
 
 
 
