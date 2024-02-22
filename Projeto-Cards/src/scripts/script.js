86c128a6f5555b6e69f35677a0debae1340c622f
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-02-21 21:04:40 -0300

    img no jquery

--- Projeto-Cards/src/scripts/script.js
+++ Projeto-Cards/src/scripts/script.js
@@ -3,27 +3,38 @@ $(document).ready(function(){
     $(".buy__hidden").hide();
 
 
+    /* FUNÇÃO DO CLICK PARA MOSTRAR OS DETALHES DOS PRODUTOS  */
     $("[id^='details-']").click(function(){
         var id = $(this).attr('id').split('-')[1];
         $("#card__hidden" + id).show();
         $('body').css('overflow', 'hidden');
     });
 
+    /*FUNÇÃO PARA O CLICK DO FECHAR */
     $(".__close").click(function(){
         $(".card__hidden").hide();
         $(".buy__hidden").hide();
         $('body').css('overflow', 'auto');
     });
 
+    /* FUNÇÃO PARA O CLICK DO COMPRAR*/
     $("[class^='card__button']").click(function(){
         $(".buy__hidden").show();
+
+        var prodImg = $(this).siblings("img").attr("src");
+        $(".buy__content-img-src").attr("src", prodImg);
+
+
     });
 
-    $("[id^='card-precco']").click(function(){
+    /*
+    $("[id^='card-preco']").click(function(){
         var id = $(this).attr('id').split('-')[1];
         $("#card__hidden" + id).show();
         $('body').css('overflow', 'hidden');
     });
+    */
+
 
 
 
