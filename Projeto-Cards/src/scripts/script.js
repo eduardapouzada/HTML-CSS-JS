5947b2a182abe6d01c9e9fd103d39178837eecc3
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-02-21 22:13:45 -0300

    form

--- Projeto-Cards/src/scripts/script.js
+++ Projeto-Cards/src/scripts/script.js
@@ -20,10 +20,14 @@ $(document).ready(function(){
     /* FUNÇÃO PARA O CLICK DO COMPRAR*/
     $("[class^='card__button']").click(function(){
         $(".buy__hidden").show();
+        $('body').css('overflow', 'hidden');
+
 
         var prodImg = $(this).siblings("img").attr("src");
-        $(".buy__content-img-src").attr("src", prodImg);
+        $(".buy__content-aside-src").attr("src", prodImg);
 
+        var prodNome = $(this).siblings(".card__title").text();
+        $(".buy__content-aside-p").text(prodNome)
 
     });
 
