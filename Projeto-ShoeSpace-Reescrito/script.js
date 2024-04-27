5e79489c10ef3a3bc56b0d5c5586404b1ca8935a
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-04-27 16:04:28 -0300

    parte do main com banners completos

---
+++ Projeto-ShoeSpace-Reescrito/script.js
@@ -0,0 +1,8 @@
+// Supondo que você tenha uma variável ou função que retorne o número de itens no carrinho
+//var numeroDeItensNoCarrinho = 3;
+
+// Selecione o elemento do contador
+//var contador = document.querySelector('.contador');
+
+// Atualize o texto do contador com o número de itens no carrinho
+//contador.textContent = numeroDeItensNoCarrinho;
