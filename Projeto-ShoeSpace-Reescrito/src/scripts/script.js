ce51e289f2208f58bb8ea0a3081def1bf7f4cfcf
Eduarda Pouzada <150561692+eduardapouzada@users.noreply.github.com>
2024-05-29 12:42:58 -0300

    adicionando a biblioca swiper para o carrossel

--- Projeto-ShoeSpace-Reescrito/src/scripts/script.js
+++ Projeto-ShoeSpace-Reescrito/src/scripts/script.js
@@ -1,11 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+//! Biblioteca Swiper, slides 
+    alert("Esta carregando o script")
+    const swiper = new Swiper(".swiper", {
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        speed: 600,
+    });
+
 //! Bloquear a rolagem em telas menores
-    document.body.classList.add('no-scroll');
+   /* document.body.classList.add('no-scroll');
     
     window.addEventListener('scroll', function() {
         window.scrollTo(0, 0);
     });
+    
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        // Adiciona uma classe ao body para aplicar estilos CSS que bloqueiem a rolagem horizontal
+        document.body.classList.add('no-horizontal-scroll');
+        
+        // Verifica e bloqueia a rolagem horizontal
+        window.addEventListener('scroll', function() {
+          // Mantém a posição vertical atual
+          let scrollTop = window.scrollY;
+          // Rola para a mesma posição vertical mas com scrollLeft igual a 0 para evitar a rolagem horizontal
+          window.scrollTo(0, scrollTop);
+        });
+      });
+
+    */
+
+    
+      
 
 //! Mudar o Placeholder em telas menores    
     function changePlaceholder() {
